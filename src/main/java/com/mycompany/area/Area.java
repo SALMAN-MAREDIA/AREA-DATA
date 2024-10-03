1305a27ef60c14c575402a6b083e059434f1af9d
SALMAN MAREDIA <SALMAN MAREDIA@LAPTOP-M7P2TFOB>
2024-10-03 19:44:28 +0530

    login page

---
+++ src/main/java/com/mycompany/area/Area.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.area;
+
+/**
+ *
+ * @author SALMAN MAREDIA
+ */
+public class Area {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
